9f4275013297c5f8b9c63362dac84d1c6ff7b399
Bradley Mark Graham <brad7777@live.com>
2022-06-04 09:05:26 +0100

    Animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,17 @@
-import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import './index.scss'
+import { useEffect, useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = [' ', 'B', 'r', 'a', 'd', 'l', 'e', 'y', ' ', 'G', 'r', 'a', 'h', 'a', 'm']
+
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi <br />I'm
-                    B.Graham
-                    <br />
-                    web developer
+                <h1>Hi, <br /> I'm
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={5}/>
                 </h1>
                 <h2>Content Developer / Ontology Expert / Teacher</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
