4ba9b8537a78c969265f976d647edbe4b486910d
unknown <brad7777@live.com>
2022-07-20 17:11:08 +0100

    colours

--- src/components/Teaching/index.js
+++ src/components/Teaching/index.js
@@ -13,24 +13,39 @@ const Teaching = () => {
     }, [] )
     return (
         <div className='content teaching-page'>
-            <section className='landing-zone'>
+            <section className='landing-zone flex-col'>
                 <h1>
                     <AnimatedLetters letterClass={letterClass} strArray={textArray} idx={5}/>
                 </h1>
                 <p>5+ years teaching and tutoring experience.</p>
-                <p>Rates starting from £20 per hour per group.</p>
-                <div className='courses'>
+                <p>Rates starting from £30 per hour per group.</p>
+                <div className='courses flex-col'>
                     <div className='chalk-outline'>
                         <h2>Available Courses</h2>
                         <ul>
-                            <li><span className='course-name'>Mathematics</span></li> 
-                            <li><span className='course-name'>English language</span></li>
-                            <li><span className='course-name'>3d-modelling with Blender</span></li>
-                            <li><span className='course-name'>game development with Roblox Studios</span></li>
-                            <li><span className='course-name'>Programming with Python</span></li>
+                            <li><a href='#mathematics' className='course-name'>Mathematics</a></li> 
+                            <li><a href='#english' className='course-name'>English language</a></li>
+                            <li><a href='#3d' className='course-name'>3d-modelling with Blender</a></li>
+                            <li><a href='#gamedev' className='course-name'>game development with Roblox Studios</a></li>
+                            <li><a href='#programming' className='course-name'>Programming with Python</a></li>
                         </ul>
                     </div>
                 </div>
+            </section>
+            <section className='mathematics-services flex-col'>
+                <h1 id='mathematics'>Mathematics</h1>
+            </section>
+            <section className='english-services flex-col'>
+                <h1 id='english'>English Language</h1>
+            </section>
+            <section className='modelling-services flex-col'>
+                <h1 id='3d'>3d Modelling</h1>
+            </section>
+            <section className='gamedev-services flex-col'>
+                <h1 id='gamedev'>Game Development</h1>
+            </section>
+            <section className='programming-services flex-col'>
+                <h1 id='programming'>Programming</h1>
             </section>
         </div>
     )
