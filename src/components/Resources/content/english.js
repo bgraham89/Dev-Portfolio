c36e02d2ff25dd0773f710b92895620c4824a4d7
Bradley Mark Graham <brad7777@live.com>
2022-07-26 22:27:45 +0100

    english

--- src/components/Resources/content/english.js
+++ src/components/Resources/content/english.js
@@ -1,10 +1,10 @@
 const English = () => {
     return (
         <div className='box flex-col'>
-            <h2>Resources for Artists</h2>
+            <h2>Resources for Fun</h2>
             <ul>
                 <li><a href="https://quickdraw.withgoogle.com/" target="_blank">Doodling activities - Google</a></li>
-                <li><a href="https://lyricstraining.com/" target="_blank">Missing word activities - LyricsTraining</a></li>
+                <li><a href="https://lyricstraining.com/" target="_blank">Missing lyrics activities - LyricsTraining</a></li>
             </ul>
         </div>
     );
